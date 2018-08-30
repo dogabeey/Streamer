3c3b458c57d44330bb24e145dfb08daea70b915a
dogabeey@gmail.com <dogabeey@gmail.com>
2018-08-30 21:34:01 +0300

    Added a frew more viewer trait.

--- Assets/Scripts/ViewerGroup.cs
+++ Assets/Scripts/ViewerGroup.cs
@@ -4,14 +4,6 @@ using UnityEngine;
 
 public class ViewerGroup : MonoBehaviour
 {
-    public string groupName;
-    public int population;
-
-    // favorites
-    public GameType[] favoriteGames;
-    public AudioClip[] favoriteSongs;
-    public Stream.PlayStyle favStyle;
-
     public enum Trait
     {
         HypeChaser, // Tends to watch popular games more.
@@ -21,8 +13,32 @@ public class ViewerGroup : MonoBehaviour
         Troll, // Very likely to scare away other groups.
         Mocker, // Enjoys poor gameplay.
         ProWatcher, // Enjoys pro gameplay.
-        Stalker // Never speaks. Hardly subs.
+        Stalker, // Never speaks. Hardly subs.
+        FemaleFollower, // Prefers to watch female streamers.
+        MaleFollower, // Prefers to watch male streamers.
+        Poor, // Never makes any donations.
+        Rich, // Their donations are generous.
+        HighEnder, // Interests in streamers with more expensive system.
+        WarezFan, // Increased sub chance to players who use their favorite manufacturer.
     }
 
-    public List<Trait> traits;
+    public string groupName;
+    public int population;
+
+    // favorites
+    public GameType[] favoriteGames;
+    public AudioClip[] favoriteSongs;
+    public Stream.PlayStyle favStyle;
+    
+    public Trait[] traits;
+
+    public ViewerGroup(string groupName, int population, GameType[] favoriteGames, AudioClip[] favoriteSongs, Stream.PlayStyle favStyle, Trait[] traits)
+    {
+        this.groupName = groupName;
+        this.population = population;
+        this.favoriteGames = favoriteGames;
+        this.favoriteSongs = favoriteSongs;
+        this.favStyle = favStyle;
+        this.traits = traits;
+    }
 }
