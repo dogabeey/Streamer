bc1f4bea980d788c69451819f939ee34df767136
dogabeey <dogabeey@gmail.com>
2018-08-30 15:01:10 +0300

    added headphone and mic

---
+++ Assets/Scripts/Hardwares/Headphone.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Headphone : Hardware
+{
+    public float maxFrequency; //kHz
+    public float sensitivity; //dB
+    public Microphone builtInMic; //undefined means none.
+
+    public Headphone(string modelName, Manufacturer manufacturer, float malfunctionRate, float cost, bool isMantadory, float maxFrequency, float sensitivity, Microphone builtInMic) : base(modelName, manufacturer, malfunctionRate, cost, isMantadory)
+    {
+        this.maxFrequency = maxFrequency;
+        this.sensitivity = sensitivity;
+        this.builtInMic = builtInMic;
+    }
+}
+
---
+++ Assets/Scripts/Hardwares/Microphone.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Microphone : Hardware
+{
+    public float maxFrequency; //kHz
+    public float sensitivity; //dB
+
+    public Microphone(string modelName, Manufacturer manufacturer, float malfunctionRate, float cost, bool isMantadory, float maxFrequency, float sensitivity) : base(modelName, manufacturer, malfunctionRate, cost, isMantadory)
+    {
+        this.maxFrequency = maxFrequency;
+        this.sensitivity = sensitivity;
+    }
+}
+
