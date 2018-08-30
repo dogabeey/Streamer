0a4e67194c35b2de92c435d1bc7f49ae747d2df3
dogabeey <dogabeey@gmail.com>
2018-08-30 17:36:39 +0300

    added viewr traits, like hype chaser or hipster etc.

--- Assets/Scripts/ViewerGroup.cs
+++ Assets/Scripts/ViewerGroup.cs
@@ -8,6 +8,19 @@ public class ViewerGroup : MonoBehaviour
     public int population;
 
     // favorites
-    public Game[] favoriteGames;
+    public GameType[] favoriteGames;
     public AudioClip[] favoriteSongs;
+    public Stream.PlayStyle favStyle;
+
+    public enum Trait
+    {
+        HypeChaser, // Tends to watch popular games more.
+        FameFollower, // Tends to watch more crowded streams.
+        Hipster, // Doesn't like popular games or streamers.
+        Loyalist, // Hardly unsubs once subbed a channel.
+        Troll,
+        Mocker,
+        ProWatcher,
+        Stalker
+    }
 }
