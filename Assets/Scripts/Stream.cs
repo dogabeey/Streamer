a0e609d2c4b78796a3c5005f229b6e52ccd0a794
dogabeey <dogabeey@gmail.com>
2018-08-30 16:56:38 +0300

    added streamer class

--- Assets/Scripts/Stream.cs
+++ Assets/Scripts/Stream.cs
@@ -2,15 +2,32 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stream : MonoBehaviour {
+public class Stream : MonoBehaviour
+{
+    public Player player;
+    [Range(0, 24)] public int startHour;
+    [Range(1, 4)] public int duration;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    public enum PlayStyle
+    {
+        Neutral,
+        Angry,
+        Cheerful,
+        Educational,
+        Humoruous,
+        Pokerface,
+        Somber
+    }
+
+    public Game game;
+    public PlayStyle playStyle;
+    public AudioClip song;
+
+    public Stream(Player player, int startHour, int duration, Game game)
+    {
+        this.player = player;
+        this.startHour = startHour;
+        this.duration = duration;
+        this.game = game;
+    }
 }
