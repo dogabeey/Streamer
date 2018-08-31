65b0b306ed3c8f42af3f84f4b735ff09cf8c43d4
dogabeey <dogabeey@gmail.com>
2018-08-31 19:07:18 +0300

    Created main menu ui template and prepared a temp background video

---
+++ Assets/Scripts/UIControl.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Video;
+using UnityEngine.UI;
+
+public class UIControl : MonoBehaviour {
+
+    public VideoPlayer bg;
+	// Use this for initialization
+	void Start ()
+    {
+        bg.
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
